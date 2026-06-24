07ac8b47fb6c77dedc385b60470a2bc9a3f66aa8
Maxim Kiyaev <92125820+gravesth@users.noreply.github.com>
2026-06-24 16:17:56 +0300

    Refactor driver entry code and improve logging

--- ProcessMemory3/driver_entry.cpp
+++ ProcessMemory3/driver_entry.cpp
@@ -2,11 +2,11 @@
 #include <ntddk.h>
 #include "driver_io.h"
 
-// наши устройства
+// устройства
 UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\datamem1");
 UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\datamem1");
 
-// тута выгрузка драйвера
+// выгрузка драйвера
 void MemoryInspectorUnload(_In_ PDRIVER_OBJECT DriverObject) {
     IoDeleteSymbolicLink(&symLink);
     IoDeleteDevice(DriverObject->DeviceObject);
@@ -15,7 +15,7 @@ void MemoryInspectorUnload(_In_ PDRIVER_OBJECT DriverObject) {
 extern "C"
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
     UNREFERENCED_PARAMETER(RegistryPath);
-    DbgPrint("kernel mode сюдааа !!");   
+    DbgPrint("kernel mode");   
     DriverObject->DriverUnload = MemoryInspectorUnload;
     DriverObject->MajorFunction[IRP_MJ_CREATE] = MemoryInspectorCreateClose;
     DriverObject->MajorFunction[IRP_MJ_CLOSE] = MemoryInspectorCreateClose;
@@ -33,19 +33,19 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
     );
 
     if (!NT_SUCCESS(status)) {
-        DbgPrint("ошибка при создании устройства((( вот с таким номером (0x%08X)\n", status);
+        DbgPrint("ошибка при создании устройства. Номер ошибки (0x%08X)\n", status);
         return status;
     }
     
-    DbgPrint("победа!! СЮДООО %wZ ", &devName);
+    DbgPrint("Done %wZ ", &devName);
 
     status = IoCreateSymbolicLink(&symLink, &devName);
     if (!NT_SUCCESS(status)) {
-        DbgPrint("символическая ссылка не создалась печалька(( (0x%08X)\n", status);
+        DbgPrint("символическая ссылка не создалась (0x%08X)\n", status);
         IoDeleteDevice(DeviceObject);
         return status;
     }
     
-    DbgPrint("символическая ссылка создалась  %wZ  WIN", &symLink);
+    DbgPrint("символическая ссылка создалась  %wZ", &symLink);
     return STATUS_SUCCESS;
 } 
