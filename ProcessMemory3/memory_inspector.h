b90ee3da376dc0666dbef4811301dc0047ea7245
Maxim Kiyaev <92125820+gravesth@users.noreply.github.com>
2026-06-24 16:21:17 +0300

    Update memory_inspector.h

--- ProcessMemory3/memory_inspector.h
+++ ProcessMemory3/memory_inspector.h
@@ -2,6 +2,5 @@
 #include <ntifs.h>
 #include <ntddk.h>
 #include "memory_region.h"
-
 // получение регионов памяти процесса
 NTSTATUS GetProcessMemoryRegions(ULONG pid, PIRP Irp, PIO_STACK_LOCATION stack); 
