efa63f4570ec2f874984a18f19d32a068e743316
Maxim Kiyaev <92125820+gravesth@users.noreply.github.com>
2026-06-24 16:16:31 +0300

    Update driver_io.h

--- ProcessMemory3/driver_io.h
+++ ProcessMemory3/driver_io.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <ntifs.h>
 #include <ntddk.h>
-// функции для работы с io окак
+// функции для работы с io 
 NTSTATUS MemoryInspectorCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
 NTSTATUS MemoryInspectorDeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp); 
